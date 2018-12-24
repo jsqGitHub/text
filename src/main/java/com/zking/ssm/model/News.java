a4d02775540fbff80bfa024dc0585fe0326210df
jsqGitHub <3119603401@qq.com>
2018-12-24 21:35:43 +0800

    123

---
+++ src/main/java/com/zking/ssm/model/News.java
@@ -0,0 +1,49 @@
+package com.zking.ssm.model;
+
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.List;
+
+@ToString
+public class News implements Serializable {
+    private Integer newsId;
+
+    private String title;
+
+    private List<Category> categoryList;
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+
+    public News(Integer newsId, String title) {
+        this.newsId = newsId;
+        this.title = title;
+    }
+
+    public News() {
+        super();
+    }
+
+    public Integer getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(Integer newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
